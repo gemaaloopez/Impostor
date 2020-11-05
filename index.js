c9c1bf6ff938efd886440343bf33fed959d6df01
gemaaloopez <gemaaloopez@hotmail.com>
2020-11-05 17:33:18 +0100

    index.js

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var bodyParser = require("body-parser");
-var modelo=require("./servidor/modelo.js")
+var io = require('socket.io').listen(server);
 
+var modelo=require("./servidor/modelo.js");
+var wss=require("./servidor/servidorWS.js");
+
+var servidorWS=new wss.ServidorWS();
 
 app.set('port', process.env.PORT || 5000);
 
@@ -14,37 +18,41 @@ app.use(bodyParser.json());
 
 var juego=new modelo.Juego();
 
-
 app.get('/', function (request, response) {
-	var contenido = fs.readFileSync(__dirname + "/cliente/index.html"); 
-	response.setHeader("Content-type", "text/html");
-	response.send(contenido);
-
+    var contenido = fs.readFileSync(__dirname + "/cliente/index.html");   
+    response.setHeader("Content-type", "text/html");
+    response.send(contenido);    
 });
 
-app.get('/crearPartida/:nick/:numero', function(request, response){
+app.get("/crearPartida/:nick/:num",function(request,response){
 	var nick=request.params.nick;
-	var num=parseInt(request.params.numero);
-	//ojo, nick nulo o numero
+	var num=parseInt(request.params.num);
+	//ojo, nick nulo o numero nulo
+	//var num=4;
 	var usr=new modelo.Usuario(nick);
-	var codigo = juego.crearPartida(num,usr);
+	var codigo=juego.crearPartida(num,usr);
 
 	response.send({"codigo":codigo});
 });
 
-app.get('/unirAPartida/:codigo/:nick', function(request, response){
+app.get("/unirAPartida/:nick/:codigo",function(request,response){
 	var nick=request.params.nick;
 	var codigo=request.params.codigo;
 	var res=juego.unirAPartida(codigo,nick);
-	response.send({res:res});
+	response.send({"res":res});
 });
 
 app.get("/listaPartidas",function(request,response){
-    var lista=juego.listaPartidas();
-    response.send(lista);
+	var lista=juego.listaPartidas();
+	response.send(lista);
 });
 
-server.listen(app.ge('port'), function () {
-	console.log('Node esta escuchando en el puerto', app.get('port'));
+server.listen(app.get('port'), function () {
+    console.log('Node esta escuchando en el puerto', app.get('port'));
 });
 
+// app.listen(app.get('port'), function () {
+//      console.log('Node app is running on port', app.get('port'));
+// });
+
+servidorWS.lanzarSocketSrv(io,juego);
